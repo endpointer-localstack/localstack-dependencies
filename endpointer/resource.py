63efa9f3ab7a8750bb923bdc3e5db58771ce2d14
Roberto Messa <wardog1@gmail.com>
2025-09-06 20:04:14 -0300

    fix: added constant

--- endpointer/resource.py
+++ endpointer/resource.py
@@ -1,3 +1,7 @@
+RESOURCE_NOT_DEPLOYED = 'resource-not-published'
+RESOURCE_NOT_PUBLISHED = 'resource-not-published'
+RESOURCE_NOT_FOUND = 'resource-not-found'
+
 def get_resource_token(request_uri):
 
     resource_token = request_uri[1]
